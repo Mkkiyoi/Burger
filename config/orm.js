86118c0775909c9262748ddcc0b2530c9d83f646
mkkiyoi <mkkiyoi@gmail.com>
2019-11-20 15:05:48 -0800

    Added connection queries to get all rows from BurgerDB, insert a new burger, and update a burger's devoured state.

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,19 @@
 let connection = require('connection');
 
 let selectAll = () => {
-
+    connection.query('SELECT * FROM Burgers', (error, results, fields) => {
+        if (error) throw error;
+    });
 }
 
-let selectAll = () => {
-    
+let insertOne = (burger_name) => {
+    connection.query('INSERT INTO Burgers (BurgerName) VALUES (?)', [burger_name], (error, results, fields) => {
+        if (error) throw error;
+    });
 }
 
-let selectAll = () => {
-    
+let updateOne = (devoured, burger_name) => {
+    connection.query('UPDATE Burgers SET Devoured=? WHERE BurgerName=?', [devoured, burger_name], (error, results, fields) => {
+        if (error) throw error;
+    });
 }
