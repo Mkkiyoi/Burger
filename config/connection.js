271c250ed80dcee2db0e84a1de482f0bdb0791e3
mkkiyoi <mkkiyoi@gmail.com>
2019-12-04 18:17:27 -0800

    Edited connection.js to include connection to JawsDB

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,18 @@ let mysql = require('mysql');
 require('dotenv').config();
 let keys = require('../keys');
 
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: keys.CREDENTIALS.id,
-    password: keys.CREDENTIALS.secret,
-    database: "BurgerDB"
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: keys.CREDENTIALS.id,
+        password: keys.CREDENTIALS.secret,
+        database: "BurgerDB"
+    });    
+}
 
 connection.connect((err) => {
     if (err) {
