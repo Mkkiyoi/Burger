eb87ea6dd2b04d030d5e2241cfb862a41f81368a
mkkiyoi <mkkiyoi@gmail.com>
2019-12-09 13:59:29 -0800

    Added onclick functions for creating, updating, and deleting burgers from the database.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,45 @@
 $(() => {
-    $('.change-devoured').on('click', (event) => {
+    
+    $('.create-burger').on('click', (event) => {
+        event.preventDefault();
+        let newBurger = {
+            name: $('#burgerName').val().trim()
+        };
+
+        $.ajax('/api/burgers', {
+            type: 'POST',
+            data: newBurger
+        }).then(() => {
+            console.log('Created burger: ' + id);
+            location.reload();
+        })
+    });
+
+    $('.delete-burger').on('click', (event) => {
+        event.preventDefault();
         let id = $(this).data('id');
-        $
-    })
+        $.ajax('/api/burgers/' + id, {
+            type: 'DELETE'
+        }).then(() => {
+            console.log('Deleted burger: ' + id);
+            location.reload();
+        });
+    });
+    
+    $('.update-devoured').on('click', (event) => {
+        event.preventDefault();
+        let id = $(this).data('id');
+        let newDevoured = $(this).data('newdevoured');
+        let newDevouredState = {
+            devoured: newDevoured
+        }
+        $.ajax('/api/burgers/' + id, {
+            type: 'PUT',
+            data: newDevouredState
+        }).then(() => {
+            console.log('Updated burger: ' + id);
+            location.reload();
+        });
+    });
+    
 });
