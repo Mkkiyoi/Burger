61f2d4a5afd9bdf743953bb7a7695b3b654c6f3b
mkkiyoi <mkkiyoi@gmail.com>
2019-12-09 18:20:37 -0800

    added returns to callbacks in orm.js and burger.js

--- models/burger.js
+++ models/burger.js
@@ -3,22 +3,22 @@ let orm = require('../config/orm');
 let burger = {
     all: function(cb) {
         orm.all("burgers", function(results) {
-            cb(results);
+            return cb(results);
         });
     }, 
     create: function (columns, values, cb) {
         orm.all("burgers", columns, values, function(results) {
-            cb(results);
+            return cb(results);
         });
     },
     update: function (objColVals, condition, cb) {
         orm.all("burgers", objColVals, condition, function(results) {
-            cb(results);
+            return cb(results);
         });
     },
     delete: function(condition, cb) {
         orm.all("burgers", condition,  function(results) {
-            cb(results);
+            return cb(results);
         });
     }
 };
