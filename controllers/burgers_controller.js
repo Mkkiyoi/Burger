a5100d1b7c48a425d4be0dddda0f79de744c88a7
mkkiyoi <mkkiyoi@gmail.com>
2019-12-11 14:34:33 -0800

    Reverted last 2 sets of changes to use anonymous function parameters

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,23 +2,13 @@ let express = require('express');
 let router = express.Router();
 let burger = require('../models/burger');
 
-let returnStatus = function(res, res, result) {
-    if (result.changeRows === 0) {
-        return res.status(404).end();
-    } else {
-        res.status(200).end();
-    }
-};
-
-let renderData = function(req, res, data) {
+router.get('/', function(req, res) {
+  burger.all(function(data) {
     let burgerData = {
       burgers: data
     };
     res.render("index", burgerData);
-};
-
-router.get('/', function(req, res) {
-  burger.all(renderData);
+  });
 });
 
 router.post('/api/burgers', function(req, res) {
@@ -35,13 +25,24 @@ router.put('/api/burgers/:id', function(req, res) {
     let condition = 'id = ' + req.params.id;
     burger.update({
         devoured: req.body.devoured
-    }, 
-    condition, returnStatus);
+    }, condition, function(result) {
+        if (result.changeRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
 });
 
 router.delete('/api/burgers/:id', function(req, res) {
     let condition = 'id = ' + req.params.id;
-    burger.delete(condition, returnStatus);
+    burger.delete(condition, function(result) {
+        if (result.affectedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
 });
 
 module.exports = router;
